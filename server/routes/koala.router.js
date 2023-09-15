d702579075391221f95bdaaeb5e5941d44d3d461
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-15 11:06:03 -0500

    ready to push to master

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,5 +14,21 @@ const koalaRouter = express.Router();
 
 
 // DELETE
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("DELETE route in /koala with id of:", id);
+    // sanitize queryText
+    const queryText = `DELELE FROM "koalas" WHERE "id" = $1;`;
+    pool
+    .query(queryText, [id])
+    .then(() => {
+        res.sendStatus(204); // 204 no content
+      })
+    .catch((err) => {
+        console.log("error in DELETing songs from books table", err);
+        res.sendStatus(500);
+    });
+    
+})
 
 module.exports = koalaRouter;
