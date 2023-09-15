815db065e6988731763f08245d6e51555f0e6b19
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-15 13:07:28 -0500

    added appendDom function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,14 +27,43 @@ function setupClickListeners() {
   }); 
 }
 
+//ajax GET
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
   
 } // end getKoalas
 
+//ajax POST
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
+}
+
+
+function appendDom(koalas){
+  $("#viewKoalas").empty();
+
+  for (let i = 0; i < koalas.length; i += 1) {
+    let koala = koalas[i];
+    // For each book, append a new row to our table
+    $("#viewKoalas").append(`
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.age}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+        <td><button class="readyButton" data-id=${koala.id}  data-ready=${koala.ready} >${koala.ready ? "Not Ready": "Ready"}</button></td>
+        <td><button class="deleteButton" data-id=${koala.id} >Delete</button></td>
+      </tr>
+    `);
+  }
+};
+
+
+// ajax PUT
+function updateKoala(event){
+  const id = $(event.target)
 }
