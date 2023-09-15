1d05e3e0a832d5a6fd24c022fd46f1c4f3edc420
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-15 11:14:13 -0500

    created put function

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -11,7 +11,21 @@ const koalaRouter = express.Router();
 
 
 // PUT
-
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const koala = req.body;
+    let queryText;
+    console.log("UPDATE koala in /koala with id:", id);
+    queryText = `UPDATE "koalas" SET "ready" = ${koala.ready} WHERE "id" = $1;`;
+    pool.query(queryText, [id])
+    .then(() => {
+        res.sendStatus(204); // 204 no content
+      })
+      .catch((err) => {
+        console.log("error in UPDATING koala from koalas table", err);
+        res.sendStatus(500);
+    });
+})
 
 // DELETE
 
