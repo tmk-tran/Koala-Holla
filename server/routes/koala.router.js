ca9ba3a37973f121fd3388248a4b4e107d50dd50
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-15 11:29:10 -0500

    added DB connection

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,21 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require("../modules/pool");
 
 // GET
+Router.get("/", (req, res) =>{
+    console.log('GET request made to /koalas/');
+    const queryText = `SELECT * FROM "koalas";`;
 
+    pool
+    .query(queryText)
+    .then((response)=> res.send(response.rows))
+    .catch((err) => {
+        console.log("Error in GET request", err);
+        res.sendStatus(500)
+    });
+});
 
 // POST
 
@@ -31,9 +42,41 @@ router.post("/", (req, res) => {
 })
 
 // PUT
-
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const koala = req.body;
+    let queryText;
+    console.log("UPDATE koala in /koala with id:", id);
+    queryText = `UPDATE "koalas" SET "ready" = ${koala.ready} WHERE "id" = $1;`;
+    pool.query(queryText, [id])
+    .then(() => {
+        res.sendStatus(204); // 204 no content
+      })
+      .catch((err) => {
+        console.log("error in UPDATING koala from koalas table", err);
+        res.sendStatus(500);
+    });
+})
 
 // DELETE
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("DELETE route in /koala with id of:", id);
+    // sanitize queryText
+    const queryText = `DELELE FROM "koalas" WHERE "id" = $1;`;
+    pool
+    .query(queryText, [id])
+    .then(() => {
+        res.sendStatus(204); // 204 no content
+      })
+    .catch((err) => {
+        console.log("error in DELETing songs from books table", err);
+        res.sendStatus(500);
+    });
+    
+})
+
+
 
 
 module.exports = koalaRouter;
