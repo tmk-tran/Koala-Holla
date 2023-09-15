99fc15f7bed5cdffb3cdb8a534744726b80e136d
jcolago <jcolago2005@gmail.com>
2023-09-15 11:07:18 -0500

    Added GET route to server

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,18 @@ const koalaRouter = express.Router();
 
 
 // GET
+Router.get("/", (req, res) =>{
+    console.log('GET request made to /koalas/');
+    const queryText = `SELECT * FROM "koalas";`;
 
+    pool
+    .query(queryText)
+    .then((response)=> res.send(response.rows))
+    .catch((err) => {
+        console.log("Error in GET request", err);
+        res.sendStatus(500)
+    });
+});
 
 // POST
 
