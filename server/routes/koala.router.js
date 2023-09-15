7c5ceb4dcefb63ebd3f6214646898b2946f6032e
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-15 10:53:15 -0500

    server side post route all set up

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -12,7 +12,22 @@ const koalaRouter = express.Router();
 router.post("/", (req, res) => {
     const koala = req.body;
     console.log("Adding Koala", koala);
-    let queryText = `INSERT INTO "koalas" ("author", "title", "isRead") `
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready", "notes") 
+                        VAlUES ($1, $2, $3, $4, $5);`
+   // backend validation
+  if (!koala.name || !koala.gender || !koala.age || !koala.ready || !koala.notes) {
+  res.sendStatus(400);
+  return;
+  } 
+  pool
+  .query(queryText, [koala.name, koala.gender, koala.age, koala.ready, koala.notes])
+  .then((result) => {
+    res.sendStatus(201);
+  })
+  .catch((error) => {
+    console.log(`Error adding new Koala`, error);
+    res.sendStatus(500);
+  }); 
 })
 
 
